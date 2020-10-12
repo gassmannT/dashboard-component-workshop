4758d22cecf70fe15639f73ff0931ea6b38d534d
Thomas Gassmann <thomas.gassmann@windowslive.com>
2020-10-12 15:24:08 +0200

    change standanlone weather tile

--- projects/weather-tile/src/app/weather-tile/weather-tile.component.ts
+++ projects/weather-tile/src/app/weather-tile/weather-tile.component.ts
@@ -5,19 +5,35 @@ import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-weather-tile',
-  templateUrl: 'weather-tile.component.html'
+  templateUrl: 'weather-tile.component.html',
+  styles: [
+    `
+      .mat-card {
+        background-color: white;
+      }
+
+      .mat-card-header-text {
+        margin: 0 !important;
+        font-weight: bold !important;
+      }
+    `
+  ],
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class WeatherTileComponent implements OnChanges {
   @Input()
   location: string;
 
-  foo$: Observable<WeatherForecast>;
+  weatherForecast$: Observable<WeatherForecast>;
 
-  constructor(private cdr: ChangeDetectorRef, private weatherService: WeatherService) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private weatherService: WeatherService
+  ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes.location) {
-      this.foo$ = this.weatherService.get(this.location);
+    if (changes.location) {
+      this.weatherForecast$ = this.weatherService.get(this.location);
     }
   }
 }
